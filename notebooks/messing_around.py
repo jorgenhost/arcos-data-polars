3892d0111eb85bcd66186fe0c679d08d76500fd5
Jørgen Høst <jorgenhost96@gmail.com>
2025-12-23 18:28:40 +0100

    csv-duckdb-polars-pq

--- notebooks/messing_around.py
+++ notebooks/messing_around.py
@@ -1,7 +1,8 @@
 # %%
 import os
 import sys
-
+os.environ['POLARS_MAX_THREADS'] = '8'
+import polars.selectors as cs
 import duckdb
 import numpy as np
 import polars as pl
@@ -10,10 +11,107 @@ NOTEBOOK_DIR =  os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.dirname(NOTEBOOK_DIR)
 DATA_DIR = os.path.join(PROJECT_ROOT, 'data')
 sys.path.append(PROJECT_ROOT)
+
+# %%
+con = duckdb.connect()
+con.execute('SET memory_limit = "8GB"') 
+con.execute('SET threads TO 8')
+
+SCHEMA = {
+    "reporter_bus_act": pl.Categorical,
+        "reporter_name": pl.Categorical,
+        "reporter_state": pl.Categorical,
+        'reporter_county': pl.Categorical,
+        "buyer_bus_act": pl.Categorical,
+        "buyer_state": pl.Categorical,
+        "drug_name": pl.Categorical,
+        "drug_code": pl.Categorical,
+        "measure": pl.Categorical,
+        "revised_company_name": pl.Categorical,
+        "ingredient_name": pl.Categorical,
+        "unit": pl.Categorical,
+        "reporter_family": pl.Categorical,
+        "combined_labeler_name": pl.Categorical,
+        "order_form_no": pl.String,
+        "dosage_unit": pl.Float64,
+        'buyer_city': pl.Categorical,
+        "transaction_code": pl.Categorical,
+        "buyer_zip": pl.Int32,
+        "reporter_zip": pl.Int32,
+        "quantity": pl.Int32,
+        "action_indicator": pl.Categorical,
+        "correction_no": pl.Float64,
+        'transaction_id': pl.Int64
+}
+
+#  %%
+lf = (
+    con.sql(
+        f"""
+        SELECT * FROM read_csv('{DATA_DIR}/raw/arcos-full.csv', 
+        header=True, 
+        quote = '"',
+        escape = '"',
+        strict_mode=false,
+        sample_size = 2000000
+        )
+        """
+    )
+    .pl(lazy=True, batch_size=10_000_000)
+    .with_columns(
+     reporter_dea_no = pl.col("reporter_dea_no").hash(),
+     buyer_dea_no = pl.col("buyer_dea_no").hash(),
+     ndc_no = pl.col("ndc_no").hash()
+    )
+    .cast(SCHEMA)
+)
+
+lf.sink_parquet(f'{DATA_DIR}/raw/arcos.pq')
+# %%
+lf.select(pl.col("buyer_zip")).collect().to_series()
+
+#%%
+lf = pl.scan_parquet(f'{DATA_DIR}/raw/arcos.pq')
+lf.head().collect()
+# %%
+lf = pl.scan_csv(f'{DATA_DIR}/raw/arcos-full.csv', ignore_errors=True).with_row_index().filter(pl.col("index")==500_000)
+lf.collect()
+# %%
+stringz = lf.select(cs.by_dtype(pl.String)).collect_schema().names()
+
+for col in stringz:
+    try:
+        s = lf.select(pl.col(col)).collect().to_series().cast(pl.Int64)
+        s = s.shrink_dtype()
+        lf = lf.with_columns(
+            col = s
+        )
+        print(f'{col}')
+    except Exception:
+        pass
+        print(f'error @ {col}')
+
+lf.head().collect()
+
+# %%
+lf.select(pl.col("reporter_zip")).collect().to_series().cast(pl.Int64)
+
 # %%
 lf = pl.scan_parquet(f'{DATA_DIR}/raw/arcos.pq')
 lf.head().collect()
 
+# %%
+con.execute(f"""
+    CREATE TABLE data AS 
+    SELECT * FROM read_csv('{DATA_DIR}/raw/arcos-full.csv', 
+        ignore_errors=True, 
+        rejects_table='errors')
+        
+""")
+
+# Look at what failed:
+bad_rows = con.sql("SELECT * FROM errors LIMIT 5").pl()
+print(bad_rows)
 
 # %%
 con = duckdb.connect()
@@ -22,10 +120,10 @@ con.execute('SET threads TO 4')
 
 lf =con.sql(f'SELECT * FROM read_parquet("{DATA_DIR}/raw/arcos2.pq")').pl(lazy=True)
 
-lf.explode("readings", "readings2").filter((pl.col("readings").is_between(7.5, 25)) | (pl.col("readings").is_between(33.43,66.89))).with_columns(year = pl.col("transaction_date").dt.year()).group_by('year', 'buyer_state').agg(avg_drugz = (pl.col("mme_conversion_factor")*pl.col("dos_str")*pl.col("quantity")*pl.col("dosage_unit")).mean()).with_columns(buyer_state = pl.col("buyer_state").cast(pl.Categorical)).sink_parquet('out1.pq')
+lf.explode("readings", "readings2").filter((pl.col("readings").is_between(7.5, 25)) | (pl.col("readings").is_between(33.43,66.89))).with_columns(year = pl.col("transaction_date").dt.year()).group_by('year', 'buyer_state').agg(avg_drugz = (pl.col("mme_conversion_factor")*pl.col("dos_str")*pl.col("quantity")*pl.col("dosage_unit")).mean()).sort('buyer_state', 'year').with_columns(buyer_state = pl.col("buyer_state").cast(pl.Categorical)).sink_parquet('out1.pq')
 # %%
 lf = pl.scan_parquet(f"{DATA_DIR}/raw/arcos2.pq")
-lf.explode("readings", "readings2").filter((pl.col("readings").is_between(7.5, 25)) | (pl.col("readings").is_between(33.43,66.89))).with_columns(year = pl.col("transaction_date").dt.year()).group_by('year', 'buyer_state').agg(avg_drugz = (pl.col("mme_conversion_factor")*pl.col("dos_str")*pl.col("quantity")*pl.col("dosage_unit")).mean()).sink_parquet('out2.pq')
+lf.explode("readings", "readings2").filter((pl.col("readings").is_between(7.5, 25)) | (pl.col("readings").is_between(33.43,66.89))).with_columns(year = pl.col("transaction_date").dt.year()).group_by('year', 'buyer_state').agg(avg_drugz = (pl.col("mme_conversion_factor")*pl.col("dos_str")*pl.col("quantity")*pl.col("dosage_unit")).mean()).sort('buyer_state', 'year').sink_parquet('out2.pq')
 
 
 
--- src/00_parse_data.py
+++ src/00_parse_data.py
@@ -1,16 +1,24 @@
 import os
+import duckdb
+import polars as pl
+import utils
 SRC_DIR =  os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.dirname(SRC_DIR)
 DATA_DIR = os.path.join(PROJECT_ROOT, 'data')
-import polars as pl
-import utils
+
 
 csv_file = f'{DATA_DIR}/raw/arcos-full.csv'
-#initial_file_size = utils.get_file_size(csv_file)
+initial_file_size = utils.get_file_size(csv_file)
 
-lf_temp = pl.scan_csv(f'{DATA_DIR}/raw/arcos-full.csv', 
-schema_overrides={
-        "reporter_bus_act": pl.Categorical,
+
+con = duckdb.connect()
+
+con = duckdb.connect()
+con.execute('SET memory_limit = "8GB"') 
+con.execute('SET threads TO 8')
+
+schema = {
+    "reporter_bus_act": pl.Categorical,
         "report_name": pl.Categorical,
         "reporter_state": pl.Categorical,
         "buyer_bus_act": pl.Categorical,
@@ -32,31 +40,38 @@ schema_overrides={
         "action_indicator": pl.Categorical,
         "correction_no": pl.Float64,
         "ndc_no": pl.String,
-    },
-    null_values=[""])
+}
 
-out_temp = f'{DATA_DIR}/raw/arcos_temp.pq'
-lf_temp.sink_parquet(out_temp, engine='streaming')
-
-
-lf = (pl.scan_parquet(f'{DATA_DIR}/raw/arcos_temp.pq', low_memory=True)
-.with_columns(
+lf = (
+    con.sql(f"""
+        SELECT * FROM read_csv('{DATA_DIR}/raw/arcos-full.csv', 
+            all_varchar=True, 
+            sample_size=-1)
+    """)
+    .pl(lazy=True)
+    .select(pl.all().name.to_lowercase())
+    .with_columns(
+        pl.all().replace_strict({"": None})
+    )
+    .with_columns(
      transaction_date = pl.col("transaction_date").str.to_date(format="%m/%d/%Y"),
      reporter_dea_no = pl.col("reporter_dea_no").hash(),
-     buyer_dea_no = pl.col("buyer_dea_no").hash())
+     buyer_dea_no = pl.col("buyer_dea_no").hash()
+    )
 )
+
 outfile = f'{DATA_DIR}/raw/arcos.pq'
 
 lf.sink_parquet(outfile, engine='streaming')
 
 gzip_csv_file = f'{DATA_DIR}/raw/arcos-full.csv.gz'
-remove_files = [gzip_csv_file, csv_file, out_temp]
+remove_files = [gzip_csv_file, csv_file]
 
 for file in remove_files:
     os.remove(file)
     print(f'Removed {file}')
 
-#final_file_size = utils.get_file_size(outfile)
+final_file_size = utils.get_file_size(outfile)
 
-#print('Optimized datatypes and converted ARCOS to parquet.')
-#print(f'Initial file size: {initial_file_size} GB. File size after optimizing: {final_file_size} GB')
+print('Optimized datatypes and converted ARCOS to parquet.')
+print(f'Initial file size: {initial_file_size} GB. File size after optimizing: {final_file_size} GB')
